695857956184cbe5f5de091079bff5b735fefd7e
jorgro <joergen.rosager@gmail.com>
2019-03-12 08:01:29 +0100

    d

--- Lab6-SMTPMailClient-students/SMTPClient.py
+++ Lab6-SMTPMailClient-students/SMTPClient.py
@@ -5,6 +5,8 @@ from socket import *
 import argparse as ap
 import getpass as gp
 import time
+import ssl
+import base64
 
 #Get sender_email and recipient_email as arguments to the program
 parser = ap.ArgumentParser(description='A test SMTP client without authentication')
@@ -28,13 +30,13 @@ endmsg = "\r\n.\r\n"
 # Our mail server is smtp.stud.ntnu.no but it allows only authenticated communications. (optional exercise)
 #mailserver = 'smtp.stud.ntnu.no'
 # You can run a local simple SMTP server such as "Fake SMTP Server" and communicate with it without authentication.
-mailserver = 'localhost'
+mailserver = 'smtp.office365.com'
 
 # Create socket called clientSocket and establish a TCP connection
 # (use the appropriate port) with mailserver
 #Fill in start
 clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((mailserver, 25))
+clientSocket.connect((mailserver, 587))
 #Fill in end
 
 recv = clientSocket.recv(1024).decode()
@@ -42,8 +44,6 @@ print(recv)
 if recv[:3] != '220':
 	print('220 reply not received from server.')
 
-# Send HELO command and print server response.
-# Can use EHLO instead since HELO is obsolete, but the latter can still be used
 heloCommand = 'EHLO Hey\r\n'
 clientSocket.send(heloCommand.encode()) #Python 3
 #clientSocket.send(heloCommand) #Python 2.7
@@ -52,25 +52,65 @@ print(recv)
 if recv[:3] != '250':
 	print('250 reply not received from server.')
 
+#TLS/SSL
+
+command = "STARTTLS\r\n"
+clientSocket.send(command.encode())
+recv = clientSocket.recv(1024)
+print(recv.decode())
+clientSocketSSL = ssl.wrap_socket(clientSocket)
+
+
+# Send HELO command and print server response.
+# Can use EHLO instead since HELO is obsolete, but the latter can still be used
+heloCommand = 'EHLO Hey\r\n'
+clientSocketSSL.send(heloCommand.encode()) #Python 3
+#clientSocket.send(heloCommand) #Python 2.7
+recv = clientSocketSSL.recv(1024).decode()
+print(recv)
+if recv[:3] != '250':
+	print('250 reply not received from server.')
+
+#authenticate
+authCommand = 'AUTH LOGIN\r\n'
+clientSocketSSL.send(authCommand.encode()) #Python 3
+#clientSocket.send(heloCommand) #Python 2.7
+recv = clientSocketSSL.recv(1024).decode()
+print(recv)
+
+authCommand = 'jorgro@stud.ntnu.no\r\n'
+clientSocketSSL.send(base64.b64encode(authCommand.encode())) #Python 3
+#clientSocket.send(heloCommand) #Python 2.7
+#recv = clientSocketSSL.recv(1024).decode()
+#print(recv)
+
+authCommand = 'hoplite1\r\n'
+clientSocketSSL.send(base64.b64encode(authCommand.encode())) #Python 3
+#clientSocket.send(heloCommand) #Python 2.7
+#recv = clientSocketSSL.recv(1024).decode()
+#print(recv)
+
+
+
 # Send MAIL FROM command and print server response.
 # Fill in start
 mailFromCommand= 'MAIL FROM:'+ fromMail +'\r\n'
-clientSocket.send(mailFromCommand.encode())
+clientSocketSSL.send(mailFromCommand.encode())
 # Fill in end
 
 # Send RCPT TO command and print server response.
 # Fill in start
 rcptCommand= 'RCPT TO:' + toMail +'\r\n'
-clientSocket.send(rcptCommand.encode())
-recv = clientSocket.recv(1024).decode()
+clientSocketSSL.send(rcptCommand.encode())
+recv = clientSocketSSL.recv(1024).decode()
 print(recv)
 # Fill in end
 
 # Send DATA command and print server response.
 # Fill in start
 dataCommand= 'DATA\r\n'
-clientSocket.send(dataCommand.encode())
-recv = clientSocket.recv(1024).decode()
+clientSocketSSL.send(dataCommand.encode())
+recv = clientSocketSSL.recv(1024).decode()
 print(recv)
 # Fill in end
 
@@ -79,24 +119,24 @@ print(recv)
 subj = "im a hacker"
 date = time.strftime("%d/%m/%Y")
 msg = "From: %s\nTo: %s\nSubject: %s\nDate: %s\n\n%s"%(fromMail, toMail, subj, date, message_text)
-clientSocket.send(msg.encode())
-recv = clientSocket.recv(1024).decode()
+clientSocketSSL.send(msg.encode())
+recv = clientSocketSSL.recv(1024).decode()
 print(recv)
 # Fill in end
 
 # Message ends with a single period.
 # Fill in start
-clientSocket.send(endmsg.encode())
-recv = clientSocket.recv(1024).decode()
+clientSocketSSL.send(endmsg.encode())
+recv = clientSocketSSL.recv(1024).decode()
 print(recv)
 # Fill in end
 
 # Send QUIT command and get server response.
 # Fill in start
 quitCommand = "QUIT'\r\n'"
-clientSocket.send(quitCommand.encode())
+clientSocketSSL.send(quitCommand.encode())
 
-recv = clientSocket.recv(1024).decode()
+recv = clientSocketSSL.recv(1024).decode()
 print(recv)
 # Fill in end
 
